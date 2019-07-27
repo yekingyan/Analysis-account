53bb1facd58de76b739953f62542400e565ffab8
yekingyan <529616@gmail.com>
2019-07-28 01:09:42 +0800

    refactor(expend.view expend.model) 重构一波就很pythonic 各颗粒度合计与流水

--- models/bill.py
+++ models/bill.py
@@ -1,4 +1,8 @@
 from datetime import date
+from copy import copy
+
+import pandas as pd
+
 from data.connet_db import get_db, query_db
 
 
@@ -17,16 +21,41 @@ class BILL:
 )"""
     columns = ['id', 'create_time', 'pay_date', 'transaction_type', 'transaction_item', 'amount', 'payment', 'pay_type',
                'need']
+    accuracy = 3  # 计算精度
+    start_pay_date = None
+    end_pay_date = None
+    data = None
+    df_data = None
+    types_columns = None
+    month_field = 'pay_month'  # 指代月份的列名
 
     def __init__(self, start_pay_date: date, end_pay_date: date):
         self.start_pay_date = start_pay_date
         self.end_pay_date = end_pay_date
+        # origin data
         self.data = self.get_range_day_data(start_pay_date, end_pay_date)
+        self.df_data = pd.DataFrame(self.data)
+        # columns
+        self.types_columns = self._get_types_columns()
+        self.day_columns = ['pay_date', 'amount', *self.types_columns]
+        self.month_columns = [self.month_field, 'amount', *self.types_columns]
+
+    def __bool__(self):
+        if self.df_data.empty:
+            return False
+        else:
+            return True
 
     @staticmethod
     def create_bill():
         """创建表"""
         get_db().execute(BILL.create_bill_text)
+
+    def _get_types_columns(self) -> list:
+        """各消费类型"""
+        if not self:
+            return []
+        return self.df_data['transaction_type'].drop_duplicates().tolist()
 
     @staticmethod
     def get_range_day_data(start_pay_date: date, end_pay_date: date) -> (dict, None):
@@ -40,18 +69,79 @@ class BILL:
         """
         return query_db(query, (start_pay_date, end_pay_date))
 
-    @classmethod
-    def day_interval(cls, start: date, end: date) -> (dict, None):
+    def get_amount_of_granularity(self, granularity: str, need_columns: list) -> pd.DataFrame:
+        """
+        根据不同的颗粒度，返回相应的类型（col）所对应的amount(row)
+        :param granularity: 数据分组的颗粒度，需在self.df_data的列名中
+        :param need_columns: 需要保留的列
+
+        pay_month   amount     交通     ...
+        4           6527.14    734.3    ...
+        5           7021.42    630.0    ...
+        """
+        df = self.df_data
+        if df.empty or granularity not in self.df_data:
+            raise Exception('无效的granularity 或 时间区间内数据为空')
+
+        # granularity时间内  各个类型 对应的 amount
+        df_transaction_type = df.groupby([granularity, 'transaction_type'])['amount'].sum().reset_index()
+        df_transaction_type = df_transaction_type.pivot(index=granularity, columns='transaction_type',
+                                                        values='amount').reset_index().round(self.accuracy)
+        # granularity时间内 总amount
+        df_amount_of_granularity = df.groupby(granularity)['amount'].sum().reset_index().round(self.accuracy)
+
+        # 合并总计与类型分类
+        df_amount_and_types = pd.merge(df_amount_of_granularity, df_transaction_type,
+                                       how='left',
+                                       left_on=df_amount_of_granularity[granularity],
+                                       right_on=df_transaction_type[granularity],
+                                       suffixes=('', '_y')
+                                       )
+        # 保留列
+        [df_amount_and_types.drop(col, axis=1, inplace=True) for col in list(df_amount_and_types) if col not in need_columns]
+        return df_amount_and_types
+
+    def daily_amount_by_types(self) -> pd.DataFrame:
         """
         以天为颗粒度
         总数，各类型数
         """
-        data = cls.get_range_day_data(start, end)
+        granularity = 'pay_date'
+        need_columns = self.day_columns
+        df_data = self.get_amount_of_granularity(granularity, need_columns)
+        return df_data
 
-    def month_interval(self, start: date, end: date) -> (dict, None):
+    def monthly_amount_by_types(self) -> pd.DataFrame:
         """
         以月为颗粒度
         总数，各类型数
         """
-        ...
+        granularity = self.month_field
+        need_columns = self.month_columns
+        self.df_data[self.month_field] = pd.to_datetime(self.df_data['pay_date']).dt.month.astype(str)
+        df_data = self.get_amount_of_granularity(granularity, need_columns)
+        return df_data
+
+    def total_amount_by_types(self) -> pd.DataFrame:
+        """类型合计金额"""
+        df = self.df_data
+        df_transaction_type = df.groupby(['transaction_type'])['amount'].sum().reset_index().round(self.accuracy)
+        df_transaction_type = df_transaction_type.rename(columns={'transaction_type': '类型'})
+        return df_transaction_type
+
+    def total_amount_by_types_merge_eat(self, total_amount_by_types: pd.DataFrame) -> dict:
+        """
+        合并多个餐饮数据
+        :param total_amount_by_types:  self.total_amount_by_types的结果
+        :return:
+        """
+        type_of_data = {k: v for k, v in total_amount_by_types.values}
+        type_of_data['amount'] = float(total_amount_by_types['amount'].sum().round(self.accuracy))
+        type_of_data['total_eat'] = 0
+        for k, v in copy(type_of_data).items():
+            if '餐饮' in k:
+                type_of_data['total_eat'] += v
+                type_of_data.pop(k)
+        type_of_data['total_eat'] = round(type_of_data['total_eat'], self.accuracy)
+        return type_of_data
 
--- routes/expend.py
+++ routes/expend.py
@@ -1,23 +1,11 @@
-import copy
-from datetime import (
-    datetime,
-    date,
-    timedelta,
-)
-
 import pandas as pd
 import numpy as np
 from flask import (
     Blueprint,
     render_template,
-    jsonify,
     request,
     abort,
 )
-from data.connet_db import (
-    query_db,
-)
-
 from libs.response import template_or_json
 from models.bill import BILL
 from forms.expend import DaysForm, MonthsForm
@@ -33,74 +21,33 @@ def hello_world():
     return render_template('test.html')
 
 
-@expend.route('/a/')
-def hello_world1():
-    query = """
-        SELECT *
-        FROM bills
-        WHERE create_time >= ?
-    """
-    create_time = datetime(year=2019, month=6, day=1)
-    data = query_db(query, (create_time,))
-    for row in data:
-        print(row['create_time'], row['amount'])
-        print(type(row['create_time']), type(row['amount']))
-    return jsonify(data)
-
-
 @expend.route('/days/')
 def days():
     form = DaysForm(request.args)
     start_date = form.data['start_date']
     end_date = form.data['end_date']
 
-    data = BILL.get_range_day_data(start_date, end_date)
-    df = pd.DataFrame(data)
-    if df.empty:
+    bill = BILL(start_date, end_date)
+    if not bill:
         abort(404, '该周期内无相应数据 \n like /expend/days/?start_date=2019/7/10&end_date=2019/7/20')
 
-    # 各个类型 amount list
-    transaction_types = df['transaction_type'].drop_duplicates().tolist()
-    df_transaction_type = df.groupby(['pay_date', 'transaction_type'])['amount'].sum().reset_index()
-    df_transaction_type = df_transaction_type.pivot(index='pay_date', columns='transaction_type',
-                                                    values='amount').reset_index()
-    # 总amount
-    df_pay_date = df.groupby('pay_date')['amount'].sum().reset_index()
-
-    # 合并总计与类型分类
-    df_data = pd.merge(df_pay_date, df_transaction_type,
-                       how='left',
-                       left_on=df_pay_date['pay_date'],
-                       right_on=df_transaction_type['pay_date'],
-                       suffixes=('', '_y')
-                       )
-    # 去nan
-    df_data = df_data.replace({np.nan: None})
+    df_data = bill.daily_amount_by_types()
+    # 时间只保留日期。去nan
     df_data['pay_date'] = df_data['pay_date'].str.slice(0, -9)
-    # df_data['amount'] = np.around(df_data['amount'].astype(np.double), 3)
-    # 保留列
-    need_columns = ['pay_date', 'amount', *transaction_types]
-    [df_data.drop(col, axis=1, inplace=True) for col in list(df_data) if col not in need_columns]
-    # 转dict
+    df_data = df_data.replace({np.nan: None})
+    # 折线图与table数据
     data = df_data.to_dict(orient="records")
 
-    # 各类型金额合计
-    type_of_data = df_data.sum().to_dict()
-    type_of_data.pop('pay_date')
-
+    # 各类型合计数
+    df_types_amount = bill.total_amount_by_types()
     # 饼图
-    pie_list = [{'类型': k, 'amount': v} for k, v in type_of_data.items() if k != 'amount']
-
-    # 大类型合计
-    type_of_data['total_eat'] = 0
-    for k, v in copy.copy(type_of_data).items():
-        if '餐饮' in k:
-            type_of_data['total_eat'] += v
-            type_of_data.pop(k)
+    pie_list = df_types_amount.to_dict('records')
+    # 大类型(选择性合并几个小类型)合计数， 详情
+    type_of_data = bill.total_amount_by_types_merge_eat(df_types_amount)
 
     return template_or_json(request, 'days.html', data={
         'rows': data,
-        'columns': need_columns,
+        'columns': bill.day_columns,
         'count': len(df_data),
         'pie': pie_list,
         'type_of_data': type_of_data,
@@ -113,55 +60,28 @@ def months():
     start_date = form.data['start_month']
     end_date = form.data['end_month']
 
-    data = BILL.get_range_day_data(start_date, end_date)
-    df = pd.DataFrame(data)
-    if df.empty:
+    bill = BILL(start_date, end_date)
+    if not bill:
         abort(404, '该周期内无相应数据 \n like /expend/months/?start_month=2019/6&end_month=2019/7')
 
-    df['pay_month'] = pd.to_datetime(df['pay_date']).dt.month.astype(str)
-    # 各个类型 amount list
-    transaction_types = df['transaction_type'].drop_duplicates().tolist()
-    df_transaction_type = df.groupby(['pay_month', 'transaction_type'])['amount'].sum().reset_index()
-
-    df_transaction_type = df_transaction_type.pivot(index='pay_month', columns='transaction_type',
-                                                    values='amount').reset_index()
-    # 总amount
-    df_pay_month = df.groupby('pay_month')['amount'].sum().reset_index()
-
-    # 合并总计与类型分类
-    df_data = pd.merge(df_pay_month, df_transaction_type,
-                       how='left',
-                       left_on=df_pay_month['pay_month'],
-                       right_on=df_transaction_type['pay_month'],
-                       suffixes=('', '_y')
-                       )
+    # 时间粒度合计数
+    df_data = bill.monthly_amount_by_types()
     # 去nan
     df_data = df_data.replace({np.nan: None})
-    # df_data['pay_month'] = df_data['pay_month'].str.slice(0, -9)
-    # df_data['amount'] = np.around(df_data['amount'].astype(np.double), 3)
-    # 保留列
-    need_columns = ['pay_month', 'amount', *transaction_types]
-    [df_data.drop(col, axis=1, inplace=True) for col in list(df_data) if col not in need_columns]
-    # 转dict
+    # 折线图与table
     data = df_data.to_dict(orient="records")
 
-    # 各类型金额合计
-    type_of_data = df_data.sum().to_dict()
-    type_of_data.pop('pay_month')
-
+    # 各类型合计数
+    df_types_amount = bill.total_amount_by_types()
     # 饼图
-    pie_list = [{'类型': k, 'amount': v} for k, v in type_of_data.items() if k != 'amount']
+    pie_list = df_types_amount.to_dict('records')
 
-    # 大类型合计
-    type_of_data['total_eat'] = 0
-    for k, v in copy.copy(type_of_data).items():
-        if '餐饮' in k:
-            type_of_data['total_eat'] += v
-            type_of_data.pop(k)
+    # 大类型(选择性合并几个小类型)合计数， 详情
+    type_of_data = bill.total_amount_by_types_merge_eat(df_types_amount)
 
     return template_or_json(request, 'days.html', data={
         'rows': data,
-        'columns': need_columns,
+        'columns': bill.month_columns,
         'count': len(df_data),
         'pie': pie_list,
         'type_of_data': type_of_data,
